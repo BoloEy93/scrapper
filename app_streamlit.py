476df9c9f9697bf4f3364d8e6740f406fd2d0498
Boris Ey. <67276238+BoloEy93@users.noreply.github.com>
2025-04-18 14:35:07 +0100

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -90,4 +90,4 @@ if st.button("Récupérer et Afficher les résultats de recherche (Ambulance)"):
             st.error(SCRAPED_DATA["error"])
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
+    uvicorn.run(app, host="0.0.0.0", port=8502, workers=1)
